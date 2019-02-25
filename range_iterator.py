992ac9fe92d6362f8d7228c3806fbd46afdad5f9
Alisa1195 <1195alisa@gmail.com>
2019-02-25 19:24:10 +0300

    range_iterator v1

---
+++ range_iterator.py
@@ -0,0 +1,23 @@
+class MyRange:
+
+    def __init__(self, start, stop, step):
+        self.current_val = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.current_val <= (self.stop - self.step):
+            self.current_val = self.current_val + self.step
+            return self.current_val
+
+
+def my_range(start, stop, step):
+    counter = start
+    call = MyRange(start, stop, step)
+    print(start)
+    while counter < (stop - step):
+        counter += step
+        print(next(call))
