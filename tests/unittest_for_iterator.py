d60fd9f97e32da7d0b6c819be71db7ac0865fdb1
Alisa1195 <1195alisa@gmail.com>
2019-02-26 15:54:26 +0300

    tests_iterator_the_simplest

---
+++ tests/unittest_for_iterator.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+import sys
+import inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+import range_iterator
+from range_iterator import *
+
+
+class TestIterator(unittest.TestCase):
+
+    def test_my_range(self):
+        iterator = range_iterator.MyRange(0, 10, 2)
+        self.assertEqual(2, next(iterator))
+
+
+unittest.main()
